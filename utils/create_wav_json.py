475d1ab84da9507289474c7f14f975e601ca22d8
KwekuYamoah <kwekuandyamoah@gmail.com>
2025-04-08 14:33:33 +0000

    Wav json added

--- utils/create_wav_json.py
+++ utils/create_wav_json.py
@@ -10,18 +10,17 @@ def change_mp3_path_to_json(input_file="data/ser_audio_features_mp3.json", outpu
     # clear output file
     with open(output_file, 'w') as f:
         f.write('')
-
+    new_data = {}
     for key in data.keys():
         # change the mp3 path to wav path for the key
+        new_key = key.replace('.mp3', '.wav').replace("_mp3", "_wav")
         
-        if key.endswith('.mp3'):
-            new_key = key.replace('movie_audio_segments_wav', 'movie_audio_segments_mp3').replace('.mp3', '.wav')
-            data[new_key] = data.pop(key)
-            print(f"Changed {key} to {new_key}")
+        # new key gets the value of the old key
+        new_data[new_key] = data[key]
         
 
     with open(output_file, 'w') as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
+        json.dump(new_data, f, indent=2, ensure_ascii=False)
 
 if __name__ == "__main__":
     change_mp3_path_to_json()
