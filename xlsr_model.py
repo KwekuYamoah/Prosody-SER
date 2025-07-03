1b1085cf1418b5a12d705b55079959e22bcc3580
sasudavid <34894599+sasudavid@users.noreply.github.com>
2025-07-03 17:13:37 +0200

    Update xlsr_model.py
    Added with the updated ASR head.

--- xlsr_model.py
+++ xlsr_model.py
@@ -15,10 +15,12 @@ from transformers import Wav2Vec2Config
 import os
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+torch.set_printoptions(threshold=float('inf'))
 
 class Wav2Vec2MTL(nn.Module):
     def __init__(self, 
                  config, 
+                 tokenizer,
                  pretrained_wav2vec_model, 
                  emotion_model_output_size, 
                  asr_model_output_size,
@@ -30,12 +32,14 @@ class Wav2Vec2MTL(nn.Module):
         self.wav2vec2_model = pretrained_wav2vec_model
         self.prosodic_prominence_model_output_size = prosodic_prominence_model_output_size
         self.pad_id = config.pad_token_id
+        self.tokenizer = tokenizer
 
         #initialize the dropout value of the network
         self.dropout = nn.Dropout(self.config.final_dropout)
 
         #initialize the ASR Component Head of the MTL model
         self.asr_head = nn.Linear(self.config.hidden_size, asr_model_output_size)
+        #self.asr_head = pretrained_wav2vec_model.lm_head
 
         #initialize the Prosodic Prominence Annotation Head of the MTL Model
         self.prosody_model_bilstm = nn.LSTM(self.config.hidden_size, prosody_model_lstm_hidden_size, batch_first=True, bidirectional=True, num_layers=1)
@@ -44,46 +48,40 @@ class Wav2Vec2MTL(nn.Module):
         #initialise the Emotion Prediction Model Head of the MTL Model
         self.emotion_prediction_head = nn.Linear(self.config.hidden_size, emotion_model_output_size)
 
+        #nn.init.kaiming_uniform_(self.asr_head.weight, a=5**0.5)
+
+        #nn.init.constant_(self.asr_head.bias, -1.0)
+
     
     def freeze_xlsr_feature_encoder(self):
         '''
         Freezes the encoder of the xlsr model
-
-        Params:
-            None
-        
-        Returns:
-            None
         '''
-        for param in self.wav2vec2_model.feature_extractor.parameters():
-            param.requires_grad = False
-
+        self.wav2vec2_model.freeze_feature_encoder()
         return
     
     def freeze_xlsr_params(self):
         '''
         Freezes all of the parameters of the xlsr model.
-
-        Params:
-            None
-        
-        Returns:
-            None
         '''
         for param in self.wav2vec2_model.parameters():
             param.requires_grad = False
-
         return
     
     def unfreeze_xlsr_model(self):
         for param in self.wav2vec2_model.parameters():
             param.requires_grad = True
-
         #continue freezing the feature encoder
-        self.freeze_xlsr_feature_encoder()
-        
+        #self.freeze_xlsr_feature_encoder()
         return
+
     
+    def randomly_initialize_asr_model_head(self):
+        '''
+        Randomly initializes the weights of the ASR and Prosody Labelling heads.
+        '''
+        self.asr_head.reset_parameters()
+        return
     
 
     def forward(self,
@@ -95,20 +93,21 @@ class Wav2Vec2MTL(nn.Module):
                 mtl_head=['asr']):
     
         #obtain the shared audio feature representations
-        w2v2_model_outputs = self.wav2vec2_model(audio_features, attention_mask=attention_mask)
-        extracted_audio_feature_representations = w2v2_model_outputs[0]
-        #print('w2v2 size: ', extracted_audio_feature_representations.size())
+        w2v2_model_outputs = self.wav2vec2_model(audio_features, attention_mask=attention_mask, output_hidden_states=True)
+
+        extracted_audio_feature_representations = w2v2_model_outputs.hidden_states[-1]
 
         #perform a dropout on the extracted audio feature representations
         processed_extracted_audio_feature_representations = self.dropout(extracted_audio_feature_representations)
 
-
         #ASR HEAD
         #push the processed extracted audio representations into the ASR head to generate logits for the ASR labels
         asr_logits = self.asr_head(processed_extracted_audio_feature_representations)
+       
 
         #generate probability values from the generated asr logits by using the softmax function
         asr_prob_labels = log_softmax(asr_logits, dim=-1)
+
 
         #EMOTION RECOGNITION HEAD
         #perform a mean pool of the processed extracted features from the w2v2 model
@@ -121,9 +120,6 @@ class Wav2Vec2MTL(nn.Module):
         #PROSODIC PROMINENCE HEAD
         #push the processed extracted features into the bilstm model
         bilstm_model_hidden_states, (hn, cn) = self.prosody_model_bilstm(processed_extracted_audio_feature_representations)
-
-        # perform a mean pool of the hidden state
-        #mean_pool_bilstm_hidden_states = bilstm_model_hidden_states.mean(dim=1)
 
         # Transpose to (B, 2*hidden, T_feat) so that we can pool over time
         bilstm_out_t = bilstm_model_hidden_states.transpose(1, 2)
@@ -150,24 +146,29 @@ class Wav2Vec2MTL(nn.Module):
         if 'asr' in mtl_head:
             #compute the asr head loss using CTC
             log_probs = asr_prob_labels.transpose(0, 1)
-            
-            input_lengths = torch.full((log_probs.size(1),), log_probs.size(0), dtype=torch.long)
-
+            conv_output_len = self.wav2vec2_model._get_feat_extract_output_lengths(attention_mask.sum(dim=1))
+            input_lengths = conv_output_len.to(torch.long)
             target_lengths = (asr_labels != self.pad_id).sum(dim=1)
-
-            ctc_loss = CTCLoss(blank=self.config.blank_token, zero_infinity=True)
-
+            ctc_loss = CTCLoss(blank=0, zero_infinity=True)
             asr_loss = ctc_loss(log_probs, asr_labels, input_lengths, target_lengths)
+
+            #regularize the loss by penalizing for the number of blanks and delimiters predicted
+            blank_log_probs = log_probs[..., 0]
+            blank_probs = blank_log_probs.exp()
+            mean_blank_probs = blank_probs.mean()
+            
+            gamma = 50
+            regularizer = gamma * (mean_blank_probs)
+            regularized_asr_loss = asr_loss + regularizer
+
 
         if 'prosodic_prominence_annotation' in mtl_head:
             bce_loss = BCEWithLogitsLoss()
-
             prosody_loss = bce_loss(prosodic_prominence_label_logits, prosodic_prominence_annotation_labels.float())
 
         
         if 'ser' in mtl_head:
             ce_loss = CrossEntropyLoss()
-
             ser_loss = ce_loss(emotion_logits, emotion_labels)
         
 
@@ -176,7 +177,7 @@ class Wav2Vec2MTL(nn.Module):
                     "asr_output": asr_prob_labels,
                     "prosody_logits": prosodic_prominence_label_logits,
                     "emotion_logits": emotion_logits,
-                    "asr_loss": asr_loss,
+                    "asr_loss": regularized_asr_loss,
                     "prosody_loss": prosody_loss,
                     "ser_loss": ser_loss
                 }
